2af29cbb5b293ff0f8a15e401d64d34f7dec712a
PoriyaMohitRaWan <83800079+PoriyaMohitRaWan@users.noreply.github.com>
2021-07-24 22:29:21 +0530

    Update install_termux.sh

--- install_termux.sh
+++ install_termux.sh
@@ -2,15 +2,14 @@ apt update&&apt upgrade
 apt update&&apt upgrade
 apt install figlet
 apt install toilet
-apt install lolcat
 pip3 install -r requirements.txt
 pip install -r requiremets.txt
 cd ..
 chmod -R 755 Tbomb/
 cd Tbomb/
 ./TBomb.sh
-cp -rf tbomb.sh ../usr/bin/tbomb
-cp -rf tbomb.sh ../usr/bin/Tbomb
+cp -rf tbomb.sh ../../usr/bin/tbomb.sh
+cp -rf tbomb.sh ../../usr/bin/Tbomb.sh
 echo '''*************************************************************'''
 echo '''**  Now You Can Run Tbomb By Simply Typing tbomb or Tbomb  **'''
 echo '''*************************************************************'''
