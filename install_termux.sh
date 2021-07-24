902cfdf7a6771de1a69165e84b3e19cc0bd471fa
PoriyaMohitRaWan <83800079+PoriyaMohitRaWan@users.noreply.github.com>
2021-07-24 22:36:43 +0530

    Update install_termux.sh

--- install_termux.sh
+++ install_termux.sh
@@ -1,9 +1,13 @@
+cd
 apt update&&apt upgrade
 apt update&&apt upgrade
 apt install figlet
 apt install toilet
 pip3 install -r requirements.txt
 pip install -r requiremets.txt
+git clone https://github.com/xero/figlet-fonts.git
+cd figlet-fonts
+cp -rf * ../../usr/share/figlet/
 cd ..
 chmod -R 755 Tbomb/
 cd Tbomb/
