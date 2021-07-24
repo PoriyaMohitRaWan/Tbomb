fba7366dec49d172c9141bd063f1bf352a041d9c
PoriyaMohitRaWan <83800079+PoriyaMohitRaWan@users.noreply.github.com>
2021-07-24 22:42:48 +0530

    Update install_debian.sh

--- install_debian.sh
+++ install_debian.sh
@@ -1,11 +1,17 @@
 apt install figlet
 apt install lolcat
+pip3 install -r requirements.txt
+pip install -r requirements.txt
+cd $HOME
+git clone https://github.com/xero/figlet-fonts.git
+cd figlet-fonts
+sudo cp -rf * /usr/share/figlet/
 cd $HOME
 chmod -R 755 Tbomb/
 cd Tbomb/
 ./TBomb.sh
-cp -rf tbomb.sh /usr/bin/tbomb
-cp -rf tbomb.sh /usr/bin/Tbomb
-echo '''********************************************************************'''
-echo '''**  Now You Can Run Tbomb From Anywhere By Typing tbomb or Tbomb  **'''
-echo '''********************************************************************'''
+cp -rf tbomb.sh /usr/bin/tbomb.sh
+cp -rf tbomb.sh /usr/bin/Tbomb.sh
+echo '''**************************************************************************'''
+echo '''**  Now You Can Run Tbomb From Anywhere By Typing tbomb.sh or Tbomb.sh  **'''
+echo '''**************************************************************************'''
